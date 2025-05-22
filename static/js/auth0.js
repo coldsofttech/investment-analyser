9d7d35e15afd44f445ca3e55a68d0a1b8cb9da9b
coldsofttech <coldsofttech@users.noreply.github.com>
2025-05-22 18:22:43 +0000

    deploy: 601d0f91c18a7509e414c0f92cc661ade6aeecb2

--- static/js/auth0.js
+++ static/js/auth0.js
@@ -1,52 +1,8 @@
 let auth0Client = null;
 
-const auth0Config = {
-    domain: "coldsofttech.uk.auth0.com",
-    clientId: "ftVRPjwMbmDUAxI8hyy0qMAakFF3FkpP",
-    redirectUri: window.location.origin + "/investment-analyser/menu.html"
-};
-
-console.log(auth0Config);
-
-async function initAuth0() {
-    auth0Client = await auth0.createAuth0Client(auth0Config);
-}
-
-async function login() {
-    try {
-        await auth0Client.loginWithRedirect();
-    } catch (err) {
-        const msg = encodeURIComponent(err.message || "Login failed");
-        window.location.href = `index.html?error=${msg}`;
-    }
-}
-
-async function logout() {
-    auth0Client.logout({
-        returnTo: window.location.origin + "/investment-analyser/index.html"
+const configureAuth0Client = async() => {
+    auth0Client = await auth0.createAuth0Client({
+        domain: "coldsofttech.uk.auth0.com",
+        clientId: "ftVRPjwMbmDUAxI8hyy0qMAakFF3FkpP"
     });
-}
-
-async function handleRedirectCallbackIfPresent() {
-    if (window.location.search.includes("code=") && window.location.search.includes("state=")) {
-        try {
-            await auth0Client.handleRedirectCallback();
-            window.history.replaceState({}, document.title, "/menu.html");
-        } catch (err) {
-            const msg = encodeURIComponent(err.message || "Auth redirect error");
-            window.location.href = `index.html?error=${msg}`;
-        }
-    }
-}
-
-async function requireAuth() {
-    const isAuthenticated = await auth0Client.isAuthenticated();
-    if (!isAuthenticated) {
-        const msg = encodeURIComponent("Not Authenticated");
-        window.location.href = `index.html?error=${msg}`;
-    }
-}
-
-async function getUserInfo() {
-    return await auth0Client.getUser();
-}
+};
