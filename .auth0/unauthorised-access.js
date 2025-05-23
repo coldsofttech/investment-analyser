95edd3896d4427a6c9f887799baa32980f5b6586
coldsofttech <161084795+coldsofttech@users.noreply.github.com>
2025-05-23 17:56:33 +0100

    Update unauthorised-access.js

--- .auth0/unauthorised-access.js
+++ .auth0/unauthorised-access.js
@@ -3,9 +3,7 @@ exports.onExecutePostLogin = async (event, api) => {
     ];
 
     const userEmail = event.user.email;
-    const userEmailVerified = event.user.email_verified;
-
-    if (!userEmail || !userEmailVerified || !allowedEmails.includes(userEmail)) {
-        api.access.deny("Unauthorized, missing, or unverified email");
+    if (!userEmail || !allowedEmails.includes(userEmail)) {
+        api.access.deny("Unauthoried access!");
     }
 };
