5d764b17ed307824162f4f7af79d016d07acd169
coldsofttech <coldsofttech@users.noreply.github.com>
2025-05-25 11:20:54 +0000

    deploy: 2d6683a1db0634de9c4364b97a24e71f0d2f5ac7

--- static/js/utils.js
+++ static/js/utils.js
@@ -16,42 +16,41 @@
         } = {}) {
             $(errorElementId).hide();
             $(errorContainer).html();
+        },
+        displayError: async function(message, {
+            redirect = false,
+            redirectUrl = '',
+            source = '',
+            errorContainer = '#errorMessageContainer',
+            errorElementId = '#errorMessage'
+        } = {}) {
+            if (redirect) {
+                const urlParams = new URLSearchParams(window.location.search);
+                urlParams.set('error', message);
+
+                const queryString = urlParams.toString();
+                if (redirectUrl) {
+                    const finalUrl = redirectUrl.includes('{source}')
+                        ? redirectUrl.replace('{source}', source)
+                        : redirectUrl;
+
+                    window.location.href = `/investment-analyser/${finalUrl}?${queryString}`;
+                } else {
+                    console.warn('Redirect requested but no redirectUrl provided.');
+                }
+            } else {
+                const $error = $(errorElementId);
+                const $container = $(errorContainer);
+
+                if ($error.length) {
+                    $error.text(message).show();
+                    $container.show();
+                } else {
+                    $container.html(`
+                        <div class="alert alert-danger" role="alert" id="${errorElementId.replace('#', '')}">${message}</div>
+                    `).show();
+                }
+            }
         }
     }
 })(jQuery);
-
-async function displayError(message, {
-    redirect = false,
-    redirectUrl = '',
-    source = '',
-    errorContainer = '#errorMessageContainer',
-    errorElementId = '#errorMessage'
-} = {}) {
-    if (redirect) {
-        const urlParams = new URLSearchParams(window.location.search);
-        urlParams.set('error', message);
-
-        const queryString = urlParams.toString();
-        if (redirectUrl) {
-            const finalUrl = redirectUrl.includes('{source}')
-                ? redirectUrl.replace('{source}', source)
-                : redirectUrl;
-
-            window.location.href = `/investment-analyser/${finalUrl}?${queryString}`;
-        } else {
-            console.warn('Redirect requested but no redirectUrl provided.');
-        }
-    } else {
-        const $error = $(errorElementId);
-        const $container = $(errorContainer);
-
-        if ($error.length) {
-            $error.text(message).show();
-            $container.show();
-        } else {
-            $container.html(`
-                <div class="alert alert-danger" role="alert" id="${errorElementId.replace('#', '')}">${message}</div>
-            `).show();
-        }
-    }
-}
