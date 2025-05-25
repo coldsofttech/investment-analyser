9ec53a7c6a4c7bb8056e4a78e00a66d41fa60ee2
coldsofttech <161084795+coldsofttech@users.noreply.github.com>
2025-05-25 12:34:31 +0100

    Removed local error alert & added $.Error

--- docs/static/js/utils.js
+++ docs/static/js/utils.js
@@ -1,54 +1,56 @@
-async function loadError() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const msg = urlParams.get('error');
+(function($) {
+    $.Error = {
+        loadError: async function() {
+            const urlParams = new URLSearchParams(window.location.search);
+            const msg = urlParams.get('error');
 
-    if (msg && msg !== null && msg !== undefined) {
-        return displayError(decodeURIComponent(msg));
-    } else {
-        return removeError();
-    }
-}
+            if (msg && msg !== null && msg !== undefined) {
+                return this.displayError(decodeURIComponent(msg));
+            } else {
+                return this.removeError();
+            }
+        },
+        removeError: async function({
+            errorContainer = '#errorMessageContainer',
+            errorElementId = '#errorMessage'
+        } = {}) {
+            $(errorElementId).hide();
+            $(errorContainer).html();
+        },
+        displayError: async function(message, {
+            redirect = false,
+            redirectUrl = '',
+            source = '',
+            errorContainer = '#errorMessageContainer',
+            errorElementId = '#errorMessage'
+        } = {}) {
+            if (redirect) {
+                const urlParams = new URLSearchParams(window.location.search);
+                urlParams.set('error', message);
 
-async function removeError({
-    errorContainer = '#errorMessageContainer',
-    errorElementId = '#errorMessage'
-} = {}) {
-    $(errorElementId).hide();
-    $(errorContainer).html();
-}
+                const queryString = urlParams.toString();
+                if (redirectUrl) {
+                    const finalUrl = redirectUrl.includes('{source}')
+                        ? redirectUrl.replace('{source}', source)
+                        : redirectUrl;
 
-async function displayError(message, {
-    redirect = false,
-    redirectUrl = '',
-    source = '',
-    errorContainer = '#errorMessageContainer',
-    errorElementId = '#errorMessage'
-} = {}) {
-    if (redirect) {
-        const urlParams = new URLSearchParams(window.location.search);
-        urlParams.set('error', message);
+                    window.location.href = `/investment-analyser/${finalUrl}?${queryString}`;
+                } else {
+                    console.warn('Redirect requested but no redirectUrl provided.');
+                }
+            } else {
+                const $error = $(errorElementId);
+                const $container = $(errorContainer);
 
-        const queryString = urlParams.toString();
-        if (redirectUrl) {
-            const finalUrl = redirectUrl.includes('{source}')
-                ? redirectUrl.replace('{source}', source)
-                : redirectUrl;
-
-            window.location.href = `/investment-analyser/${finalUrl}?${queryString}`;
-        } else {
-            console.warn('Redirect requested but no redirectUrl provided.');
+                if ($error.length) {
+                    $error.text(message).show();
+                    $container.show();
+                } else {
+                    $container.html(`
+                        <div class="alert alert-danger" role="alert" id="${errorElementId.replace('#', '')}">${message}</div>
+                    `).show();
+                }
+            }
         }
-    } else {
-        const $error = $(errorElementId);
-        const $container = $(errorContainer);
-
-        if ($error.length) {
-            $error.text(message).show();
-            $container.show();
-        } else {
-            $container.html(`
-                <div class="alert alert-danger" role="alert" id="${errorElementId.replace('#', '')}">${message}</div>
-            `).show();
-        }
     }
-}
+})(jQuery);
